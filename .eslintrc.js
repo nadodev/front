5ef871a9bb2d4716d34aaf8a468de30357a21242
Leonardo Geja <68485328+nadodev@users.noreply.github.com>
2021-08-30 08:33:01 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,57 @@
 module.exports = {
   env: {
     browser: true,
+
     es6: true,
   },
+
   extends: ['airbnb', 'prettier'],
+
   parser: 'babel-eslint',
+
   globals: {
     Atomics: 'readonly',
+
     SharedArrayBuffer: 'readonly',
   },
+
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+
     ecmaVersion: 2018,
+
     sourceType: 'module',
   },
+
   plugins: ['react', 'prettier', 'react-hooks'],
+
   rules: {
-    'prettier/prettier': 'error',
     'react/jsx-filename-extension': 'off',
+
     'import/prefer-default-export': 'off',
+
     'react-hooks/rules-of-hooks': 'error',
+
     'react-hooks/exhaustive-deps': 'warn',
+
     'react/react-in-jsx-scope': 'off',
+
     'jsx-a11y/anchor-is-valid': 'off',
+
     'react/no-typos': 'off',
+
     'jsx-a11y/label-has-associated-control': 'off',
+
     'no-unused-vars': 'off',
+
     'no-shadow': 'off',
+
     'jsx-a11y/no-static-element-interactions': 'off',
+
     'consistent-return': 'off',
+
+    'linebreak-style': 0,
   },
 };
